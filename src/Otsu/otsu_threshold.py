1388441955c44925e9f0d41d53cfbf0dfd99bc89
DavidSolanas <davidsolanas086@gmail.com>
2019-12-13 20:45:49 +0100

    Otsu thresholding

--- src/Otsu/otsu_threshold.py
+++ src/Otsu/otsu_threshold.py
@@ -3,16 +3,32 @@
     Author: David Solanas Sanz
     TFG
 """
-import math
 
 import numpy as np
 
 
 def threshold(src_data, dst_img):
+    """
+    Applies an Otsu threshold to select relevant brain voxels in the image. The ones not relevant
+    are set to 0.
+
+    Parameters
+    ----------
+    src_data: ndarray
+        The source image data
+    dst_img: Nifti1Image
+        The final image that will have its data with the Otsu threshold applied
+
+    Returns
+    -------
+
+    """
+    # Dimensions of the image and the max value of the image voxels
     x, y, z, _ = src_data.shape
     top = int(src_data.max())
+    # Number of voxels in the image
     total = x * y * z
-    # Calculate histogram
+    # Calculate the histogram of the image
     histogram = np.empty(top + 1, int)
     values = np.array(range(0, top + 1))
     for i in range(0, x):
@@ -21,6 +37,7 @@ def threshold(src_data, dst_img):
                 val = int(src_data[i, j, k][0])
                 histogram[val] += 1
 
+    # Apply the Otsu threshold,  minimizing the intra-class variance is equivalent to maximizing inter-class variance
     best_threshold = 0
     sumb = .0
     wb = .0
@@ -35,10 +52,11 @@ def threshold(src_data, dst_img):
                 max_inter_var = val
                 best_threshold = _threshold
 
+        # Update wb and sumb
         wb += histogram[_threshold]
         sumb += _threshold * histogram[_threshold]
-    print(best_threshold)
 
+    # Filter the image, if a value is less than the threshold is set to 0
     for i in range(0, x):
         for j in range(0, y):
             for k in range(0, z):
