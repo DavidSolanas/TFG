eb2be32f269ee8c5fed49e34886b190de36d2b69
DavidSolanas <davidsolanas086@gmail.com>
2020-02-29 18:02:57 +0100

    Preprocessing fixed

--- src/resample.py
+++ src/resample.py
@@ -11,8 +11,9 @@ import nibabel as nib
 import numpy as np
 from nilearn import image
 from scipy import ndimage
-
-from src.Otsu import otsu_threshold
+import math
+from skimage.filters import threshold_otsu
+from skimage.morphology import erosion, dilation
 
 
 def show_slices(slices):
@@ -54,53 +55,6 @@ def show_grid(slices):
     plt.draw()
 
 
-"""
-Code from: https://github.com/nipy/nibabel/issues/670
-"""
-
-
-def rescale_affine(input_affine, voxel_dims=[1, 1, 1], target_center_coords=None):
-    """
-    This function uses a generic approach to rescaling an affine to arbitrary
-    voxel dimensions. It allows for affines with off-diagonal elements by
-    decomposing the affine matrix into u,s,v (or rather the numpy equivalents)
-    and applying the scaling to the scaling matrix (s).
-    Parameters
-    ----------
-    input_affine : np.array of shape 4,4
-        Result of nibabel.nifti1.Nifti1Image.affine
-    voxel_dims : list
-        Length in mm for x,y, and z dimensions of each voxel.
-    target_center_coords: list of float
-        3 numbers to specify the translation part of the affine if not using the same as the input_affine.
-    Returns
-    -------
-    target_affine : 4x4matrix
-        The resampled image.
-    """
-    # Inicializar target_affine
-    target_affine = input_affine.copy()
-
-    # Descomposición en valores singulares de la matriz M =  U * S * V
-    # Descompone la matriz M en la suma de productos de vectores
-    # u -> Vectores unitarios (base ortogonal), genera el espacio de filas de M.
-    # s -> Vector que contiene los valores singulares, ordenados de forma decreciente
-    # v -> Vectores unitarios (base ortogonal), genera el espacio de columnas de N
-    u, s, v = np.linalg.svd(target_affine[:3, :3], full_matrices=False)
-
-    # Reescalado de la imagen al tamaño (en mm) de los vóxeles especificado
-    s = voxel_dims
-
-    # Reconstruir target_affine, @ es el operador de producto matricial
-    target_affine[:3, :3] = u @ np.diag(s) @ v
-
-    # Set the translation component of the affine computed from the input
-    # image affine if coordinates are specified by the user.
-    if target_center_coords is not None:
-        target_affine[:3, 3] = target_center_coords
-    return target_affine
-
-
 def resample_img(source_image, target_shape, voxel_dims=[2., 2., 2.]):
     """
         This function resamples an input image to a specific (mm)
@@ -119,34 +73,26 @@ def resample_img(source_image, target_shape, voxel_dims=[2., 2., 2.]):
             The resampled image.
         """
 
-    target_affine = source_image.affine.copy()
-
-    # Calculate the translation part of the affine
-    spatial_dimensions = (source_image.header['dim'] * source_image.header['pixdim'])[1:4]
-
-    # Calculate the translation affine as a proportion of the real world
-    # spatial dimensions
-    image_center_as_prop = source_image.affine[0:3, 3] / spatial_dimensions
-
-    # Calculate the equivalent center coordinates in the target image
-    dimensions_of_target_image = (np.array(voxel_dims) * np.array(target_shape))
-
-    target_center_coords = dimensions_of_target_image * image_center_as_prop
-
-    # Readjust center coords
-    target_center_coords = source_image.affine[0:3, 3] + (source_image.affine[0:3, 3] - target_center_coords) / 2.
-
-    # target_center_coords[1] += 5
-    target_affine = rescale_affine(target_affine, voxel_dims, target_center_coords)
-
-    resampled_img = image.resample_img(img, target_affine, target_shape=target_shape)
+    voxel_transform = image.resample_img(source_image, target_affine=np.diag(voxel_dims))
 
     # Restore correct values in header
-    resampled_img.header['pixdim'] = img.header['pixdim']
-    resampled_img.header['dim'][5:] = 0
-    resampled_img.header['xyzt_units'] = 2
+    voxel_transform.header['dim'][5:] = 0
+    voxel_transform.header['pixdim'][5:] = 0
+    voxel_transform.header['xyzt_units'] = 2
 
-    return resampled_img
+    img_shape = np.array(voxel_transform.header['dim'][1:4])
+
+    target_affine = voxel_transform.affine.copy()
+    center_x = img_shape[0] // 2 - 50
+    center_y = img_shape[1] // 2 - 50
+    center_z = img_shape[2] // 2 - 45
+    target_affine[0, 3] = center_x * target_affine[0, 0] + target_affine[0, 3]
+    target_affine[1, 3] = center_y * target_affine[1, 1] + target_affine[1, 3]
+    target_affine[2, 3] = center_z * target_affine[2, 2] + target_affine[2, 3]
+
+    new_img = image.resample_img(voxel_transform, target_affine=target_affine, target_shape=target_shape)
+
+    return new_img
 
 
 def copy_data(src_dataobj, src_thresholded, dst_img):
@@ -158,38 +104,58 @@ def copy_data(src_dataobj, src_thresholded, dst_img):
                 dst_img.dataobj[i, j, k] = src_dataobj[i, j, k] if src_thresholded[i, j, k] == 1 else 0
 
 
+def get_images(path):
+    directories = os.listdir(path)
+    images = []
+    for _d in directories:
+        _path = os.path.join(path, _d)
+        if os.path.isfile(_path):
+            if _d != '.DS_Store':
+                images.append(_path)
+        else:
+            sub_images = get_images(_path)
+            images += sub_images
+
+    return images
+
+
 ####################################################
 print(sys.argv)
 
-base_dir = '/Users/david/Desktop/PET images'
-data_path = os.path.join(base_dir,
-                         'scanner_prueba2.nii')
+base = sys.argv[1]
+dest = sys.argv[2]
+src_data = get_images(base)
 
-img = nib.load(data_path)
-print(img.dataobj.shape)
-dim = (100, 100, 90)
-voxels = [2., 2., 2.]
+for data_path in src_data:
+    img = nib.load(data_path)
+    dim = (100, 100, 90)
+    voxels = [2., 2., 2.]
+    img_resampled = resample_img(img, dim, voxels)
 
-img_resampled = resample_img(img, dim, voxels)
+    # Otsu thresholding to remove noise
+    data = img_resampled.dataobj.copy()
+    thresh = threshold_otsu(data, nbins=math.ceil(img_resampled.dataobj.max()))
+    thresholded = img_resampled.dataobj > thresh
 
-threshold_map = resample_img(img, dim, voxels)
-otsu_threshold.threshold(img_resampled.dataobj, threshold_map)
+    connectivity = np.ones(shape=(3, 3))
+    # Fill holes in the brain image
+    for z in range(0, 90):
+        img_fill_holes = ndimage.binary_fill_holes(np.squeeze(thresholded[:, :, z], axis=2)).astype(int)
+        eroded = erosion(img_fill_holes, connectivity)
+        dilated = dilation(eroded, connectivity)
+        for x in range(0, 100):
+            for y in range(0, 100):
+                thresholded[x, y, z] = dilated[x, y]
 
-# Fill holes in the brain image
-for z in range(0, 90):
-    img_fill_holes = ndimage.binary_fill_holes(np.squeeze(threshold_map.dataobj[:, :, z], axis=2)).astype(int)
-    for x in range(0, 100):
-        for y in range(0, 100):
-            threshold_map.dataobj[x, y, z] = img_fill_holes[x, y]
+    copy_data(img_resampled.dataobj, thresholded, img_resampled)
 
-copy_data(img_resampled.dataobj, threshold_map.dataobj, img_resampled)
+    # Show z-axis brain image
+    slices = create_grid(img_resampled)
+    # show_grid(slices)
 
-
-# Show z-axis brain image
-slices = create_grid(img_resampled)
-show_grid(slices)
-
-# Save image
-nib.save(img_resampled, os.path.join(base_dir, 'prueba_resampled2.nii.gz'))
-
-plt.show()
+    # show_nifti(img)
+    # show_nifti(img_resampled)
+    # Save image
+    name_img = data_path.split('/')[-1] + '.gz'
+    nib.save(img_resampled, os.path.join(dest, name_img))
+    # plt.show()
