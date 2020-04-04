c4c035d65d6df5a90d4a4192607b1c6c72e17a5a
David Solanas <davidsolanas086@gmail.com>
2020-04-04 13:09:46 +0200

    inception_v3

--- src/PET_NN.py
+++ src/PET_NN.py
@@ -1,3 +1,9 @@
+"""
+    File: PET_NN.py
+    Author: David Solanas Sanz
+    TFG
+"""
+
 import os
 from tensorflow.keras import layers
 from tensorflow.keras import Model
@@ -5,7 +11,9 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.applications.inception_v3 import InceptionV3
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
+import tensorflow as tf
 
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
 
 def load_data(base_dir):
@@ -38,34 +46,34 @@ def load_data(base_dir):
         _x, _y = np.load(os.path.join(train_AD_dir, img_AD)), 0
         X_train.append(_x)
         y_train.append(_y)
-    """
+
     for img_CN in train_CN_fnames:
         _x, _y = np.load(os.path.join(train_CN_dir, img_CN)), 1
-        X_train.append([_x])
+        X_train.append(_x)
         y_train.append(_y)
 
     for img_MCI in train_MCI_fnames:
         _x, _y = np.load(os.path.join(train_MCI_dir, img_MCI)), 2
         X_train.append([_x])
         y_train.append(_y)
-    """
+
     # Test dataset
     for img_AD in validation_AD_fnames:
         _x, _y = np.load(os.path.join(validation_AD_dir, img_AD)), 0
         X_test.append(_x)
         y_test.append(_y)
-    """
+
     for img_CN in validation_CN_fnames:
         _x, _y = np.load(os.path.join(validation_CN_dir, img_CN)), 1
-        X_test.append([_x])
+        X_test.append(_x)
         y_test.append(_y)
 
     for img_MCI in validation_MCI_fnames:
         _x, _y = np.load(os.path.join(validation_MCI_dir, img_MCI)), 2
         X_test.append([_x])
         y_test.append(_y)
-    """
-    return np.array(X_train), np.array(y_train), np.array(X_test), np.array(y_test)
+
+    return X_train, y_train, X_test, y_test
 
 
 pre_trained_model = InceptionV3(include_top=False,
@@ -82,11 +90,11 @@ print('last layer output shape: ', last_layer.output_shape)
 last_output = last_layer.output
 
 # Flatten the output layer to 1 dimension
-x = layers.Dropout(rate=0.4)(last_output)
+x = layers.Flatten()(last_output)
+x = layers.Dropout(rate=0.4)(x)
 # Add a fully connected layer with 1,024 hidden units and ReLU activation
 x = layers.Dense(1024, activation='relu')(x)
 x = layers.Dense(512, activation='relu')(x)
-x = layers.Dense(3, activation='relu')(x)
 # Add a final sigmoid layer for classification
 x = layers.Dense(3, activation='softmax')(x)
 
@@ -97,8 +105,11 @@ model.compile(optimizer=Adam(lr=0.0001),
               metrics=['acc'])
 
 # Define our example directories and files
-base_dir = '/Users/david/TFG/brain_data'
+base_dir = '/content/drive/My Drive/brain_data'
 X_train, y_train, X_test, y_test = load_data(base_dir)
+
+X_train = np.array(X_train)
+X_test = np.array(X_test)
 
 X_train = np.dstack([X_train] * 3)
 X_test = np.dstack([X_test] * 3)
@@ -114,18 +125,22 @@ train_datagen = ImageDataGenerator(width_shift_range=0.1,
 # Note that the validation data should not be augmented!
 test_datagen = ImageDataGenerator()
 
-# Flow training images in batches of 20 using train_datagen generator
-train_generator = train_datagen.flow(X_train, y_train,
-                                     batch_size=8)
+batch_size = 8
+steps_per_epoch = int(len(y_train) / batch_size)
+validation_steps = int(len(y_test) / batch_size)
 
-# Flow validation images in batches of 20 using test_datagen generator
-validation_generator = test_datagen.flow(X_test, y_test,
-                                         batch_size=8)
+# Flow training images in batches of 8 using train_datagen generator
+train_generator = train_datagen.flow(X_train, y_train, shuffle=True,
+                                     batch_size=batch_size)
 
-history = model.fit_generator(
+# Flow validation images in batches of 8 using test_datagen generator
+validation_generator = test_datagen.flow(X_test, y_test, shuffle=True,
+                                         batch_size=batch_size)
+
+history = model.fit(
     train_generator,
     validation_data=validation_generator,
-    steps_per_epoch=50,
-    epochs=1,
-    validation_steps=50,
-    verbose=1)
+    epochs=10,
+    verbose=1,
+    steps_per_epoch=steps_per_epoch,
+    validation_steps=validation_steps)
